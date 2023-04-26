a2e195ecc7c5e42ba6bb1063773d67b87f6235ef
LiWin <leewenpeng@126.com>
2023-04-26 19:57:03 +0800

    测试bitset

--- LeeCode/main.cpp
+++ LeeCode/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "solution.h"
+#include <bitset>
 
 using namespace std;
 
@@ -12,6 +13,7 @@ int main(int argc, char *argv[])
     // cout << solution.hammingWeight(11) << endl;
     // cout << solution.hammingDistance_1(1, 4) << endl;
     // cout << solution.reverseBits(43261596) << endl;
+    /*
     int a[] = {1, 2, 3, 4, -1, -1, 5};
     TreeNode *root;
     initTree(root, a, 7);
@@ -24,6 +26,14 @@ int main(int argc, char *argv[])
         }
         cout << endl;
     }
+    */
+    bitset<1> flag(0);
+    flag = 1;
+    cout << flag << endl;
+    if (flag == 1)
+        cout << "111111" << endl;
+    else
+        cout << "000000" << endl;
     cout << "x²" << endl;
     return 0;
 }
